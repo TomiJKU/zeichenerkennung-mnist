2cbbc95fba25137980ce333e9b4b9c6b7785f829
TomiJKU <thomas.leitner3@gmail.com>
2026-01-16 17:04:15 +0100

    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss s ssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssss

--- training/train_emnist_byclass.py
+++ training/train_emnist_byclass.py
@@ -1,16 +1,22 @@
 import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 
-def preprocess(example):
-    x = tf.cast(example["image"], tf.float32) / 255.0  # (28,28,1)
-    y = tf.cast(example["label"], tf.int32)
+def show_sample(ds, title="Sample"):
+    for x, y in ds.take(1):
+        img = x.numpy().squeeze()     # (28,28)
+        label = int(y.numpy())        # scalar
+        plt.imshow(img, cmap="gray")
+        plt.title(f"{title} | label={label}")
+        plt.axis("off")
+        plt.show()
 
-    # Optional: "human-friendly" orientation:
-    # TFDS Hinweis: EMNIST ist gedreht/gespiegelt. 
-    # Für das Modell nicht zwingend nötig, aber okay:
-    #x = tf.transpose(x, perm=[1, 0, 2])
 
+def preprocess(x, y):
+    x = tf.cast(x, tf.float32) / 255.0
+    x = tf.transpose(x, perm=[1, 0, 2])  # 3D: (28,28,1) aktuelle version 16.01.26
+    y = tf.cast(y, tf.int32)
     return x, y
 
 def main():
@@ -18,11 +24,20 @@ def main():
     out_path = os.path.join(project_root, "models", "best_model.keras")
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
 
-    ds_train = tfds.load("emnist/byclass", split="train", as_supervised=False)
-    ds_test  = tfds.load("emnist/byclass", split="test", as_supervised=False)
+    ds_train = tfds.load("emnist/byclass", split="train", as_supervised=True)
+    ds_test  = tfds.load("emnist/byclass", split="test", as_supervised=True)
 
-    ds_train = ds_train.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE).shuffle(10_000).batch(128).prefetch(tf.data.AUTOTUNE)
-    ds_test  = ds_test.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE).batch(256).prefetch(tf.data.AUTOTUNE)
+    # map zuerst (noch 3D)
+    ds_train_mapped = ds_train.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test_mapped  = ds_test.map(preprocess,  num_parallel_calls=tf.data.AUTOTUNE)
+
+    # DEBUG: Sample nach Preprocessing (ohne batch!)
+    show_sample(ds_train_mapped, title="EMNIST after preprocessing")# einfacher:
+    # show_sample(ds_train_mapped.unbatch(), title="EMNIST after preprocessing")
+
+    # dann erst batchen
+    ds_train_final = ds_train_mapped.shuffle(10_000).batch(128).prefetch(tf.data.AUTOTUNE)
+    ds_test_final  = ds_test_mapped.batch(256).prefetch(tf.data.AUTOTUNE)
 
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(28, 28, 1)),
@@ -37,7 +52,7 @@ def main():
     ])
 
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-    model.fit(ds_train, epochs=10, validation_data=ds_test)
+    model.fit(ds_train_final, epochs=10, validation_data=ds_test_final)
     model.save(out_path)
     print("Saved:", out_path)
 
